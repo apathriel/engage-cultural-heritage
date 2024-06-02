a45ea838b7ce79ceb0fb367b57df2af764722a56
apathriel <gabrielandersen1@gmail.com>
2024-06-02 23:20:33 +0200

    Add temporary scaffold for R function implementation to be used with rpy2

---
+++ src/temp.r
@@ -0,0 +1,18 @@
+# Initialize renv virtual environment
+
+renv::init()
+
+renv::restore()
+
+# Install and load pacman for managing packages
+install.packages("pacman")
+library(pacman)
+
+# Load the necessary packages and dependencies, install if not available
+p_load(raster, dplyr, sf, here)
+
+setwd(here::here())
+
+elevation <- raster::getData("alt", country = "DNK", mask = TRUE)
+
+municipalities <- getData("GADM", country = "DNK", level = 2)
