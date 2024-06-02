da34a92e4494d56517dbf9c42e7ae697e9789b36
apathriel <gabrielandersen1@gmail.com>
2024-06-02 22:26:27 +0200

    Add logging utilities for debugging & error handling

---
+++ src/logging_utils.py
@@ -0,0 +1,22 @@
+import logging
+
+
+def get_logger(name: str) -> logging.Logger:
+    """
+    Creates and configures a logger with the specified name.
+
+    Parameters:
+        name (str): The name of the logger. Convention is to use __name__.
+
+    Returns:
+        logging.Logger: The configured logger object.
+    """
+    name = name.upper()
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter("[%(levelname)s] - [%(name)s] - %(asctime)s - %(message)s")
+    )
+    logger.addHandler(handler)
+    return logger
