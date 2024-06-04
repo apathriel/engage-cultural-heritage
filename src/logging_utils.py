19c44e16a640b62503e187c18b5629cee3319ab4
apathriel <gabrielandersen1@gmail.com>
2024-06-04 21:10:34 +0200

    Refactor: Add optional log file functionality to get_logger()

--- src/logging_utils.py
+++ src/logging_utils.py
@@ -1,12 +1,14 @@
 import logging
+from pathlib import Path
 
-
-def get_logger(name: str) -> logging.Logger:
+def get_logger(name: str, log_to_file: bool = False, log_file: str = 'logfile.log') -> logging.Logger:
     """
     Creates and configures a logger with the specified name.
 
     Parameters:
         name (str): The name of the logger. Convention is to use __name__.
+        log_to_file (bool): If True, logs are saved to a file. Otherwise, logs are output to the console.
+        log_file (str): The name of the log file. Only used if log_to_file is True.
 
     Returns:
         logging.Logger: The configured logger object.
@@ -14,7 +16,14 @@ def get_logger(name: str) -> logging.Logger:
     name = name.upper()
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
+
+    if log_to_file:
+        log_dir = Path('logs')
+        log_dir.mkdir(parents=True, exist_ok=True)
+        handler = logging.FileHandler(log_dir / log_file)
+    else:
+        handler = logging.StreamHandler()
+
     handler.setFormatter(
         logging.Formatter("[%(levelname)s] - [%(name)s] - %(asctime)s - %(message)s")
     )
