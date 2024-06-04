ab6547c970f02354d21a53431cb634db07646cee
apathriel <gabrielandersen1@gmail.com>
2024-06-04 22:29:18 +0200

    Add functionaltiy from filtering columns from shapefiles using geopandas. Update paths.

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,17 +1,50 @@
+import os
 from pathlib import Path
+from typing import List
 
+import geopandas as gpd
 import pandas as pd
 
-from data_processing_utilities import load_csv_as_df, export_df_as_csv
+from code_utilities import timing_decorator
+from data_processing_utilities import load_csv_as_df, export_df_as_csv, get_file_size
 from logging_utils import get_logger
 
-
 logger = get_logger(__name__)
+
+def filter_out_unnecessary_columns(input_path: Path, columns_to_remove: List[str], output_path: Path = None,) -> gpd.GeoDataFrame:
+    # Handle default value for output_path when not provided..
+    if output_path is None:
+        output_path = input_path
+
+    # Get the shapefile file size, just for clarity in the log message
+    shapefile_file_size = get_file_size(input_path)
+
+    logger.info(f"Attempting to load shapefile from {input_path} ({shapefile_file_size} mb). This may take some time for large files...")
+    try:
+        gdf = gpd.read_file(input_path)
+    except Exception as e:
+        logger.error(f"Error loading shapefile from {input_path}. {e}")
+        raise
+    logger.info("Shapefile loaded successfully!")
+    
+    logger.debug(f"GeoDataFrame head before removing columns: {gdf.head()}")
+
+    for column in columns_to_remove:
+        try:
+            gdf = gdf.drop(columns=column)
+        except KeyError:
+            logger.error(f"Column {column} not found in GeoDataFrame. Skipping this column.")
+    logger.debug(f"GeoDataFrame head after removing columns: {gdf.head()}")
+    
+    logger.info(f"Exporting filtered GeoDataFrame to {output_path}...")
+    gdf.to_file(output_path, driver='ESRI Shapefile')
+
+    return gdf
 
 
 def main():
     input_csv_path = (
-        Path(__file__).resolve().parents[1] / "data" / "anlaeg_all_25832.csv"
+        Path(__file__).resolve().parents[1] / "data" / "input" / "anlaeg_all_25832.csv"
     )
 
     columns = ["anlaegsbetydning"]
@@ -84,6 +117,15 @@ def main():
         directory=Path(__file__).resolve().parents[1] / "data" / "output",
         filename="anlaegsbetydning_value_counts",
     )
+
+    # Initialize input/output paths
+    shapefile_path = Path(__file__).resolve().parents[1] / "data" / "input" / "anlaeg_all_25832.shp"
+    cleaned_shapefile_output_path = Path(__file__).resolve().parents[1] / "data" / "output" / "cleaned_anlaeg_all_25832.shp"
+
+    # Define columns to filter
+    columns_to_delete = ["systemnr", "stednr", "loknr", "sbext", "frednr", "anlnr", "anlaegstyp", "dateringskode", "fra_aar", "til_aar", "kommunenavn", "kommunenr", "sevaerdighedsklasse"]
+
+    gdf = filter_out_unnecessary_columns(shapefile_path, columns_to_delete, cleaned_shapefile_output_path) 
 
     logger.info("Script completed!")
 
