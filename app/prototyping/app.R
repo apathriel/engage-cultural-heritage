f2b063f33c4a372a854afca21e227b87c48ecc3f
Gabriel Høst Andersen <94848510+apathriel@users.noreply.github.com>
2024-06-06 13:35:23 +0200

    Clean up server code, attempt (unsuccessfully) to implement modularization. Reorder and implement structures in server code. Update buffer func, remove buffer on marker move, gen buffer from search, fix update marker coords.

--- app/prototyping/app.R
+++ app/prototyping/app.R
@@ -10,6 +10,9 @@ source("data_prep_module.r")
 # Source functions for map
 source("map_functions_module.r")
 
+# Source server functions, because it was really hard to read
+# source("server_functions_module.r")
+
 # UI code
 ui <- navbarPage(
   "Engage!",
@@ -55,7 +58,7 @@ ui <- navbarPage(
               actionButton("grouped_route_button", "Generate adventure route", style = "flex: 1; margin-left: 10px;")
             ),
             div(
-              style="display: flex; justify-content: space-between;",
+              style = "display: flex; justify-content: space-between;",
               actionButton("export_routes_button", "Export route suggestions", style = "flex: 1;")
             )
           )
@@ -80,19 +83,23 @@ ui <- navbarPage(
   )
 )
 
-# Server code
+
 server <- function(input, output, session) {
-  # Reactive expression to filter data based on the selected group(s) from pickerInput
+  
+  # Reactive expressions
+  # --------------------
+  
+  # Filter data based on the selected group(s) from pickerInput
   filtered_data <- reactive({
     monuments_transformed[monuments_transformed$anlaegsbet %in% input$group, ]
   })
 
-  # Reactive expression to handle search query. Return value on event for further processing
+  # Handle search query. Return value on event for further processing
   search_query <- eventReactive(input$search_button, {
     input$search
   })
 
-  # Update search result based on search_query, which is updated from search field, check if search_query
+  # Update search result based on search_query
   search_result <- reactive({
     if (is.null(search_query())) {
       return(NULL)
@@ -101,38 +108,98 @@ server <- function(input, output, session) {
     }
   })
 
-  # Initialize a reactive value to store the marker
+  # Initialize reactive values to store the marker and buffer
   marker_id <- reactiveValues(id = NULL)
+  buffer <- reactiveValues(data = NULL)
 
-  # Add observer, first param event to observe, second is handler - if search_result is not null, set view to the returned geocoded search result and add a marker
+  
+  # Observers
+  # ---------
+  
+  # Observer for search_result
   observeEvent(search_result(), {
     if (!is.null(search_result())) {
-      leafletProxy("map") %>%
-        setView(
-          lng = search_result()[1],
-          lat = search_result()[2],
-          zoom = 17
-        )
+      # Set view to the returned geocoded search result and add a marker
+      leafletProxy("map") %>% setView(lng = search_result()[1], lat = search_result()[2], zoom = 17)
 
       # Remove the old marker
       if (!is.null(marker_id$id)) {
-        leafletProxy("map") %>%
-          removeMarker(layerId = marker_id$id)
+        leafletProxy("map") %>% removeMarker(layerId = marker_id$id)
       }
 
       # Add a marker at the search result location
-      leafletProxy("map") %>%
-        addMarkers(
-          lng = search_result()[1],
-          lat = search_result()[2],
-          popup = paste("Search result at:", search_result()[1], ",", search_result()[2]),
-          layerId = "my_marker"
-        )
-
+      leafletProxy("map") %>% addMarkers(lng = search_result()[1], lat = search_result()[2], popup = paste("Search result at:", search_result()[1], ",", search_result()[2]), layerId = "my_marker")
       marker_id$id <- "my_marker"
+
+      # Update marker_lng and marker_lat
+      marker_lng <<- search_result()[1]
+      marker_lat <<- search_result()[2]
     }
   })
 
+  # Observer for generate_buffer_Button
+  observeEvent(input$generate_buffer_Button, {
+    # Check if a marker has been placed
+    if (!is.null(marker_id$id)) {
+      # Remove the old buffer if it exists
+      if (!is.null(buffer$data)) {
+        leafletProxy("map") %>% removeShape(layerId = "my_buffer")
+      }
+
+      # Create a point from the marker's coordinates
+      point <- st_sfc(st_point(c(marker_lng, marker_lat)), crs = 4326)
+
+      # Create a buffer around the point
+      buffer$data <- st_buffer(point, dist = input$km_slider * 1000)
+    } else {
+      # Display a popup message
+      showModal(modalDialog(title = "Warning", "Please place a marker before generating a buffer!", easyClose = TRUE))
+    }
+  })
+
+  # Observer for buffer
+  observe({
+    # Check if a buffer has been created
+    if (!is.null(buffer$data)) {
+      # Convert the buffer to a data frame
+      buffer_df <- st_as_sf(buffer$data)
+
+      # Add the buffer to the map
+      leafletProxy("map") %>% addPolygons(data = buffer_df, fillColor = "blue", fillOpacity = 0.5, color = "blue", weight = 1, layerId = "my_buffer")
+    }
+  })
+
+  # Observer for map_click
+  observeEvent(input$map_click, {
+    if (input$place_marker) {
+      # Get the coordinates of the click event
+      click_coords <- input$map_click
+      lng <- click_coords$lng
+      lat <- click_coords$lat
+
+      # Remove the old marker
+      if (!is.null(marker_id$id)) {
+        leafletProxy("map") %>% removeMarker(layerId = marker_id$id)
+      }
+
+      # Remove the old buffer if it exists
+      if (!is.null(buffer$data)) {
+        leafletProxy("map") %>% removeShape(layerId = "my_buffer")
+        buffer$data <- NULL
+      }
+
+      # Add a new marker at the click location
+      leafletProxy("map") %>% addMarkers(lng = lng, lat = lat, popup = paste("Marker at:", lng, ",", lat), layerId = "my_marker")
+      marker_id$id <- "my_marker"
+      marker_lng <<- lng
+      marker_lat <<- lat
+    }
+  })
+
+  
+  # Render functions
+  # ----------------
+  
   # Render the leaflet map
   output$map <- renderLeaflet({
     # Use the reactive filtered_data() instead of the original data
@@ -140,34 +207,6 @@ server <- function(input, output, session) {
 
     # Create the map using your map_functions$create_map function
     mapbox_map <- map_functions$create_map(map_data)
-
-    observeEvent(input$map_click, {
-      if (input$place_marker) {
-        # Get the coordinates of the click event
-        click_coords <- input$map_click
-        lng <- click_coords$lng
-        lat <- click_coords$lat
-
-        # Remove the old marker
-        if (!is.null(marker_id$id)) {
-          leafletProxy("map") %>%
-            removeMarker(layerId = marker_id$id)
-        }
-
-        # Add a new marker at the click location
-        leafletProxy("map") %>%
-          addMarkers(
-            lng = lng,
-            lat = lat,
-            popup = paste("Marker at:", lng, ",", lat),
-            layerId = "my_marker"
-          )
-
-        marker_id$id <- "my_marker"
-        marker_lng <<- lng
-        marker_lat <<- lat
-      }
-    })
 
     mapbox_map
   })
